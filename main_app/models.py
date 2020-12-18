158e2f1a944051cc25819b080f9cc7d8201e1ec8
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-12-17 22:07:20 -0800

    Remove temporary route data from views. Create DB and migrate model from models.py.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Route(models.Model):
+    name = models.CharField(max_length=100)
+    grade = models.CharField(max_length=10)
+    description = models.CharField(max_length=500)
+    crag = models.CharField(max_length=50)
+    rock_type = models.CharField(max_length=50)
+    pitches = models.IntegerField()
+    
